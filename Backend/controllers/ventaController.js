889b6d3210c0807e5c73f26b4b0bc8d0b175db71
JholfrankRC <jholfrank202@gmail.com>
2022-11-09 16:25:03 -0500

    v2

---
+++ Backend/controllers/ventaController.js
@@ -0,0 +1,7 @@
+const prueba = (req, res)=>{res.send({
+    msg:"En esta ruta gestionaremos todas las peticiones correspondiente al modelo de Venta. claro que iba a salir mamalindaaa"
+    })
+};
+export {
+    prueba
+};
